28ec3ff235e462d26005206eb12ebd339b6f1f6c
“ericelvendahl” <ericelvendahl@gmail.com>
2020-08-18 15:09:05 -0500

    fizzbuzz working

---
+++ fizzbuzz.py
@@ -0,0 +1,11 @@
+x = 0
+while x < 16:
+    if x % 3 == 0 and x % 5 == 0:
+        print("fizzbuzz")
+    elif x % 3 == 0:
+        print("fizz")
+    elif x % 5 == 0:
+        print("buzz")
+    else:
+        print(x)
+    x += 1
---
+++ loops.py
@@ -0,0 +1,11 @@
+days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+print("For loop:")
+for day in days:
+    print(day)
+
+print("While loop:")
+x = 5
+while x > 0:
+    print("Counting down! I'm at " + str(x))
+    x -= 1
